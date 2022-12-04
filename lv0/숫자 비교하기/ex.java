cf8f88692acc83b155a16bbbc8b1b663ecf14c39
suji5262 <suji5262@gmail.com>
2022-12-04 23:37:04 +0900

    프로그래머스 코딩문제 add

---
+++ lv0/숫자 비교하기/ex.java
@@ -0,0 +1,14 @@
+public class ex {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+
+            if(num1 == num2) {
+                return 1;
+            }else{
+                return -1;
+            }
+
+        }
+    }
+}
